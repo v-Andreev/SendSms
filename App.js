321fcd81640e144ea908fec44f1df301a3431c20
Vlad <26vladilsav02@gmail.com>
2017-10-31 22:54:15 +0300

    Install module send sms

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
   Button,
   ToastAndroid
 } from 'react-native';
+const SmsAndroid = require('react-native-sms-android');
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -21,10 +22,31 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const sendDirect = () => {
+    SmsAndroid.sms(
+        '+375293541772', // phone number to send sms to
+        'This is the sms text', // sms body
+        'sendDirect', // sendDirect or sendIndirect
+        (err, message) => {
+            if (err){
+                console.log("error");
+                ToastAndroid.show('err' + err, ToastAndroid.SHORT);
+            } else {
+                console.log(message); // callback message
+                ToastAndroid.show(message, ToastAndroid.SHORT);
+            }
+        }
+    );
+}
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Button
+            onPress={sendDirect}
+            title="SendDirect"
+        />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
